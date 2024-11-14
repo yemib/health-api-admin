184700eecda559367011cea67a237abd4eb9ddaa
Obafemi Emmanuel <obafemie@gmail.com>
2024-11-14 07:27:53 +0100

    update the Api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,9 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            \App\Http\Middleware\cor::class
         ],
+       
     ];
 
     /**
